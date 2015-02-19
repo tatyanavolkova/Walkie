dfef9eb04b78de4dc8083ed3e2a3feba848a93e9
r545-7 <r545-7@1-244.inot-rsuh.local>
2015-02-19 14:56:03 +0300

    Merge origin/master
    Conflicts:
    	src/walkie/Walkie.java

--- src/walkie/Walkie.java
+++ src/walkie/Walkie.java
@@ -35,49 +35,9 @@ public class Walkie {
         System.out.print("\nHail's from NicolayStefanov.Praise the Sun!\n");
         System.out.print("\n Anastasyzko says: Greetings from the end of the line!\n");
         
-    }   
-   
-    
-}
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package walkie;
-
-/**
- *
- * @author r545-7
- */
-public class Walkie {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.print("Hello, I am Walkie!\n");    
-        System.out.print("Hello from Tatyana!\n");
-        System.out.print("Hello, I am jbenua!\n");
-        
-        System.out.print("CONFLICT!\n");
-        
-        System.out.print("Hello, I am Yunna!\n");
-        System.out.print("try выталкивание!\n");
-        
-        System.out.print("try сервер!\n");
-        System.out.print("hello from lena t!\n");
-        
-        System.out.print("Hello from Rinaly.\n");
-        System.out.print("Hello, I am erin!\n");
-        System.out.print("hello from aleksoider!\n");
-        System.out.print("hello from grinaram!\n");
-        System.out.print("\nHail's from NicolayStefanov.Praise the Sun!\n");
-        System.out.print("\nhello from Irina!\n");
-        
         System.out.print ("\nHello from Lenus1k! :)\n");
-        System.out.print("\nGREETINGS FROM THE HORSEMEN OF APACALIPSIS");
+        System.out.print("\nGREETINGS FROM THE HORSEMEN OF APACALIPSIS");     
+        System.out.print("\nhello from Irina!\n");
     }   
    
     
