5e6c5fdf6e3f3d9a0ed641c66b938991a0192270
r545-2 <r545-2@1-255.inot-rsuh.local>
2015-02-19 14:38:48 +0300

    Merge origin/master
    Conflicts:
    	src/walkie/Walkie.java

--- src/walkie/Walkie.java
+++ src/walkie/Walkie.java
@@ -30,42 +30,6 @@ public class Walkie {
         
         System.out.print("Hello from Rinaly.");
            System.out.print("Anastasyzko says: Let me write here something at last!");
-    }   
-}
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package walkie;
+      
 
-/**
- *
- * @author r545-7
- */
-public class Walkie {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.print("Hello, I am Walkie!");    
-        System.out.print("Hello from Tatyana!");
-        System.out.print("Hello, I am jbenua!");
-        
-        System.out.print("CONFLICT!");
-        
-        System.out.print("Hello, I am Yunna!");
-        System.out.print("try выталкивание!");
-        
-        System.out.print("try сервер!");
-        System.out.print("hello from lena t!");
-        
-        System.out.print("Hello from Rinaly.");
-        System.out.print("Hello, I am erin!");
-        
-    }   
-   
-    
-}
+    }}
