05ff0d61d958513664355bdf195609d44f2b6054
r545-8 <r545-8@1-233.inot-rsuh.local>
2015-02-19 14:22:03 +0300

    from Rina

--- src/walkie/Walkie.java
+++ src/walkie/Walkie.java
@@ -17,6 +17,7 @@ public class Walkie {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("Hello, I am Walkie!");
+        System.out.print("Hello from Rinaly.");
     }
     
 }
